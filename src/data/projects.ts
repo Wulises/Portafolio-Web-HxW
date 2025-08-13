3fabf2a57c50e453be14a2225f0896f4cad880ab
Wulises <126424439+Wulises@users.noreply.github.com>
2025-08-13 04:34:39 -0600

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ export const projects = [
     title: "Portafolio Personal",
     description:
       "Portafolio con Next.js 13, Tailwind CSS, Framer Motion y un diseño elegante inspirado en Haru de Persona 5 Royal.",
-    image: "/images/Haru.jpg", // Guarda la imagen en public/proyectos/
+    image: "/images/ima1.png", // Guarda la imagen en public/proyectos/
     techs: ["Next.js", "Tailwind CSS", "Framer Motion"],
     github: "https://github.com/Wulises/Portafolio-Web-HxW", // cámbialo por el real
     demo: "https://portafolio-web-hxw.vercel.app/", // o el link del deploy
@@ -14,7 +14,7 @@ export const projects = [
     title: "Proyecto con Vue.js y Vite",
     description:
       "Aplicación en Vue.js, Vite, Tailwind CSS, donde se pone aprueba lo básico del framework Vue.js con un toque de jardinería.",
-    image: "/images/Vivian.png",
+    image: "/images/ima2.png",
     techs: ['Vue3.js', 'Vite', 'Tailwind CSS'],
     github: "https://github.com/Wulises/vue-garden-tracker", // si no tienes link, déjalo así
     demo: "https://vue-garden-tracker.vercel.app/",
@@ -23,7 +23,7 @@ export const projects = [
   {
     title: 'Dashboard de Finanzas',
     description: 'Aplicación web para visualizar ingresos y gastos con gráfica dinámica y diseño personalizado, utilizando Next.js 13, Tailwind CSS, Zustand y Chart.js',
-    image: '/images/kasumi.jpg',
+    image: '/images/ima3.png',
     techs: ['Next.js', 'TypeScript','Tailwind CSS', 'Zustand', 'Chart.js'],
     github: 'https://github.com/Wulises/Personal-Finance-Tracker-WXM',
     demo: 'https://personal-finance-tracker-wxm.vercel.app/'
@@ -32,7 +32,7 @@ export const projects = [
   {
     title: 'Aplicación Monstruo de todo lo que es full stack',
     description: 'Aplicación web full stack con Next.js 13, Taiwind CSS, PostgreSQL, la cual muestra todo lo que se necesita para dominar el stack completo y moderno de desarrollo web.',
-    image: '/images/makoto.png',
+    image: '/images/ima4.png',
     techs: ['Next.js', 'Node.js', 'Tailwind CSS', 'PostgreSQL', 'Express'],
     github: '',
     demo: 'https://the-real-phantom-full-stack-thief-h-alpha.vercel.app/',
